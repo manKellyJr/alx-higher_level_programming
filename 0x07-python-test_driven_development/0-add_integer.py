96344d3900d0caf4ad0e04b9aeb866c000068da6
Obed Banda <obedbanda34@gmail.com>
2022-10-21 22:21:29 +0200

    Adds two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,9 @@
 
 def add_integer(a, b=98):
     """Return the integer addition of a and b.
+
     Float arguments are typecasted to ints before addition is performed.
+
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """
